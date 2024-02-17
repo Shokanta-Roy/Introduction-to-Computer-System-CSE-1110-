0220c3bc63b89902a030e1f5d54f082fcf7a8c45
Shokanta Roy <141769613+Shokanta-Roy@users.noreply.github.com>
2024-02-17 13:19:52 +0600

    Add files via upload

---
+++ Basic Introductory Problems/p10.c
@@ -0,0 +1,24 @@
+//Author: SHOKANTA ROY
+//linkedin: https://www.linkedin.com/in/shokanta-roy-746387255/
+//github: https://github.com/Shokanta-Roy/
+
+
+#include <stdio.h>
+
+int main() {
+    long int longIntVariable = 1234567890L;         // A long int variable
+    long long int longLongIntVariable = 1234567890123456789LL; // A long long int variable
+    long double longDoubleVariable = 3.141592653589793238L;  // A long double variable
+    short int shortIntVariable = 32767;              // A short int variable
+
+    // Printing the values
+    printf("The Long Int Value: %ld\n", longIntVariable);
+    printf("The Long Long Int Value: %lld\n", longLongIntVariable);
+    printf("The Long Double Value: %Lf\n", longDoubleVariable);
+    printf("The Short Int Value: %d\n", shortIntVariable);
+
+    return 0;
+}
+
+
+//please ignore the sample input-output;
