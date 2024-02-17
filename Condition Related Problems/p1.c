4ffabc28baf16992b2d50cc33a436dd60f2a9b7f
Shokanta Roy <141769613+Shokanta-Roy@users.noreply.github.com>
2024-02-17 11:49:04 +0600

    Add files via upload

---
+++ Condition Related Problems/p1.c
@@ -0,0 +1,21 @@
+//Author: SHOKANTA ROY
+//linkedin: https://www.linkedin.com/in/shokanta-roy-746387255/
+//github: https://github.com/Shokanta-Roy/
+
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n>=0)
+    {
+        printf("Positive");
+    }
+    else
+    {
+        printf("Negative");
+    }
+
+    return 0;
+}
