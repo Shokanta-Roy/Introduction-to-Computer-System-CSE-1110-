d396402bfeaef34bd47e57ea9bf154499e1494b5
Shokanta Roy <141769613+Shokanta-Roy@users.noreply.github.com>
2024-02-17 12:03:41 +0600

    Add files via upload

---
+++ Condition Related Problems/p2.c
@@ -0,0 +1,22 @@
+//Author: SHOKANTA ROY
+//linkedin: https://www.linkedin.com/in/shokanta-roy-746387255/
+//github: https://github.com/Shokanta-Roy/
+
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("Even");
+    }
+    else
+    {
+        printf("Odd");
+    }
+
+    return 0;
+}
+
