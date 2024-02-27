5cce045b2b7aecefb634519b6aaa248fccb4683a
Shokanta Roy <141769613+Shokanta-Roy@users.noreply.github.com>
2024-02-27 18:24:33 +0600

    Add files via upload

---
+++ Operator Related Problems/p2.c
@@ -0,0 +1,15 @@
+//Author: SHOKANTA ROY
+//linkedin: https://www.linkedin.com/in/shokanta-roy-746387255/
+//github: https://github.com/Shokanta-Roy/
+
+#include<stdio.h>
+#define PI 3.1416
+int main()
+{
+    float r,area;
+    scanf("%f",&r);
+    area= 2*PI*r;
+    printf("Area: %.1f",area);
+
+    return 0;
+}
---
+++ Operator Related Problems/p3.c
@@ -0,0 +1,16 @@
+//Author: SHOKANTA ROY
+//linkedin: https://www.linkedin.com/in/shokanta-roy-746387255/
+//github: https://github.com/Shokanta-Roy/
+
+#include<stdio.h>
+#define PI 3.1416
+int main()
+{
+    float a,b,x;
+    scanf("%f %f",&a,&b);
+    x=(3.31 *(a * a)+2.01 * (b * b * b))/(7.16 * (b * b)+2.01 * (a * a *a));
+    printf("X= %.5f",x);
+
+    return 0;
+}
+
