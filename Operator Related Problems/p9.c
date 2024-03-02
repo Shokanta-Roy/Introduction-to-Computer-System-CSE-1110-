1a662e7d0befd8d33b1ef092a4b7d90a526a4519
Shokanta Roy <141769613+Shokanta-Roy@users.noreply.github.com>
2024-03-02 13:59:38 +0600

    Add files via upload

---
+++ Operator Related Problems/p9.c
@@ -0,0 +1,19 @@
+//Author: SHOKANTA ROY
+//linkedin: https://www.linkedin.com/in/shokanta-roy-746387255/
+//github: https://github.com/Shokanta-Roy/
+
+
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c,X,Y,Z;
+    scanf("%d%d%d",&a,&b,&c);
+    X = a - b / 3 + c * 2 - 1;
+    Y = a - ( b / ( 3 + c ) * 2) - 1;
+    Z = a - ( ( b / 3) + c * 2) - 1;
+
+    printf("X=%d\nY=%d\nZ=%d",X,Y,Z);
+
+    return 0;
+}
