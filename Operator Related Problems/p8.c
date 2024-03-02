64f8eb29d88d423ae84df8d2948e14588101ff6f
Shokanta Roy <141769613+Shokanta-Roy@users.noreply.github.com>
2024-03-02 13:30:49 +0600

    Add files via upload

---
+++ Operator Related Problems/p8.c
@@ -0,0 +1,15 @@
+//linkedin: https://www.linkedin.com/in/shokanta-roy-746387255/
+//github: https://github.com/Shokanta-Roy/
+
+
+#include<stdio.h>
+
+int main()
+{
+    int x,y,max;
+    scanf("%d %d",&x,&y);
+    max = (x > y) ? x : y;
+    printf("%d",max);
+
+    return 0;
+}
