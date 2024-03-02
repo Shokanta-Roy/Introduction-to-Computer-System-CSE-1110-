5c81e757763e37e8606a993981c8d32b5077845a
Shokanta Roy <141769613+Shokanta-Roy@users.noreply.github.com>
2024-03-02 12:40:24 +0600

    Add files via upload

---
+++ Operator Related Problems/p1.c
@@ -0,0 +1,27 @@
+//Author: SHOKANTA ROY
+//linkedin: https://www.linkedin.com/in/shokanta-roy-746387255/
+//github: https://github.com/Shokanta-Roy/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int number1, number2;
+
+    // Input the expression from the user
+    scanf("%d %d", &number1,  &number2);
+
+
+    printf("Addition: %d\n", number1+number2);
+
+    printf("Subtraction: %d\n", number1-number2);
+
+    printf("Multiplication: %d\n", number1*number2);
+
+    printf("Reminder: %d\n", number1%number2);
+
+    printf("Quotient: %d\n", number1/number2);
+
+    return 0;
+}
