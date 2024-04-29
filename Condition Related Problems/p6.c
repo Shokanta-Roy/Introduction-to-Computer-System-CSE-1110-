f9b47ba08b7e3672c499b57d92f5d30a3976274e
Shokanta Roy <141769613+Shokanta-Roy@users.noreply.github.com>
2024-04-29 12:08:25 +0600

    Add files via upload

--- Condition Related Problems/p6.c
+++ Condition Related Problems/p6.c
@@ -10,26 +10,51 @@ int main()
     int num;
     scanf("%d", &num);
 
-    if (num == 0)
-    {
-        printf("Zero is not a valid input.");
-        return 0;
-    }
-    else if(num<= -1)
-    {
-        printf("Negative input is not valid");
-        return 0;
-    }
+////    if (num == 0)
+////    {
+////        printf("Zero is not a valid input.");
+////        return 0;
+////    }
+////    else if(num<= -1)
+////    {
+////        printf("Negative input is not valid");
+////        return 0;
+////    }
+////
+////    // Check if the number is a power of 2
+////    //We use the bitwise AND operation num & (num - 1) to check if the number is a power of 2.
+////    //If the result is zero, it means that the number is a power of 2.
+////    //This is because powers of 2 have only one bit set, and subtracting 1 from a power of 2 flips all the bits to the right of the least significant bit (LSB), leaving the LSB unchanged.
+////    //Thus, performing a bitwise AND with the original number results in zero.
+////    if ((num & (num - 1)) == 0)
+////        printf("Yes\n", num);
+////    else
+////        printf("No\n", num);
 
-    // Check if the number is a power of 2
-    //We use the bitwise AND operation num & (num - 1) to check if the number is a power of 2.
-    //If the result is zero, it means that the number is a power of 2.
-    //This is because powers of 2 have only one bit set, and subtracting 1 from a power of 2 flips all the bits to the right of the least significant bit (LSB), leaving the LSB unchanged.
-    //Thus, performing a bitwise AND with the original number results in zero.
-    if ((num & (num - 1)) == 0)
-        printf("Yes\n", num);
+    //we can do it without using the bitwise AND operation.
+    if (num > 0)
+    {
+        while (num % 2 == 0)
+        {
+            num /= 2;
+        }
+        if (num == 1)
+        {
+            printf("Yes\n");
+        }
+        else
+        {
+            printf("No\n");
+        }
+    }
+    else if (num == 0)
+    {
+        printf("Zero is not a valid input.\n");
+    }
     else
-        printf("No\n", num);
+    {
+        printf("Negative input is not valid.\n");
+    }
 
     return 0;
 }
